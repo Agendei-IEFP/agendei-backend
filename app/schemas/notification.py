9f89be4fb9b3fb2e183b639af1dd02cda184d0c8
IsaqueHenrique1 <isaque.do.palmares@gmail.com>
2026-05-11 01:08:19 +0100

    fix: str id's fixed and import enums from app.model

--- app/schemas/notification.py
+++ app/schemas/notification.py
@@ -1,66 +1,43 @@
 from datetime import datetime
-from pydantic import BaseModel, Field
-from typing import Optional, Literal, Any
-from pydantic import ConfigDict
-from enum import Enum
+from pydantic import BaseModel, ConfigDict
+from typing import Optional
+from app.models.notification import (
+    NotificationType,
+    RecipientType,
+    NotificationChannel,
+    NotificationStatus,
+    )
 
 
-class NotificationType(str, Enum):
-    reminder = "reminder"
-    confirmation = "confirmation"
-    cancellation = "cancellation"
-    evaluation = "evaluation"
-
-
-class RecipientType(str, Enum):
-    customer = "customer"
-    professional = "professional"
-
-
-class NotificationChannel(str, Enum):
-    email = "email"
-    sms = "sms"
-    whatsapp = "whatsapp"
-
-
-# -----------------------------
 # Base
-# -----------------------------
 class NotificationBase(BaseModel):
-    type: NotificationType
+    notification_type: NotificationType
     channel: NotificationChannel
-    recipient_id: int
+    recipient_id: str
     recipient_type: RecipientType
-    appointment_id: Optional[int] = None
+    appointment_id: Optional[str] = None
     title: Optional[str] = None
     message: str
-    payload: Optional[dict[str, Any]] = None #JSON para guardar outros dados da notificação
     scheduled_at: datetime #Quando a notificação deve ser enviada
 
 
-# -----------------------------
 # Create
-# -----------------------------
 class NotificationCreate(NotificationBase):
     pass
 
 
-# -----------------------------
 # Update
-# -----------------------------
 class NotificationUpdateStatus(BaseModel):
-    status: Optional[str] = None
+    status: Optional[NotificationStatus] = None
     attempts: Optional[int] = None
     error_message: Optional[str] = None
     sent_at: Optional[datetime] = None
 
 
-# -----------------------------
 # Public (API response)
-# -----------------------------
 class NotificationPublic(NotificationBase):
-    id: int
-    status: str
+    id: str
+    status: NotificationStatus
     attempts: int
     error_message: Optional[str] = None
     sent_at: Optional[datetime] = None
@@ -70,16 +47,15 @@ class NotificationPublic(NotificationBase):
     model_config = ConfigDict(from_attributes=True)
 
 
-# -----------------------------
 # List view
-# -----------------------------
 class NotificationList(BaseModel):
-    id: int
-    type: str
+    id: str
+    notification_type: str
     channel: str
-    recipient_id: int
+    recipient_id: str
+    appointment_id: Optional[str] = None
     recipient_type: str
-    status: str
+    status: NotificationStatus
     scheduled_at: datetime
     sent_at: Optional[datetime] = None
 
