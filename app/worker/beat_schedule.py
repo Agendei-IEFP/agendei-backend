e244fdadeb7694ef364f6419c4a4e4c58c353ebf
IsaqueHenrique1 <isaque.do.palmares@gmail.com>
2026-06-01 00:21:46 +0100

    feat: add beat schedule

--- app/worker/beat_schedule.py
+++ app/worker/beat_schedule.py
@@ -1,9 +1,15 @@
 from celery.schedules import crontab
-from app.worker.celery_app import celery_app
 
-celery_app.conf.beat_schedule = {
+beat_schedule = {
+    # Envio imediato continua a cada 60s
     "dispatch-scheduled-notifications": {
         "task": "app.worker.tasks.dispatch_pending_notifications",
-        "schedule": 60.0,  # a cada 60 segundos
+        "schedule": 1.0,
+    },
+
+    # Verificação diária às 08:00
+    "daily-pending-check": {
+        "task": "app.worker.tasks.dispatch_pending_notifications",
+        "schedule": crontab( minute="*"),
     },
 }
