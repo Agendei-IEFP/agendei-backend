277ee9fef993bdbbca959a14f158d873e715d88a
IsaqueHenrique1 <isaque.do.palmares@gmail.com>
2026-06-01 00:23:24 +0100

    feat: add celery configuration

--- app/worker/celery_app.py
+++ app/worker/celery_app.py
@@ -1,17 +1,24 @@
 from celery import Celery
 from app.core.config import settings
 
-celery_app = Celery(
-    "notifications",
+# 1) Criar o Celery app
+celery = Celery(
+    main="notifications",
     broker=settings.REDIS_URL,
     backend=settings.REDIS_URL,
     include=["app.worker.tasks"],
 )
 
-celery_app.conf.update(
+# 2) Configurações gerais
+celery.conf.update(
     task_serializer="json",
     result_expires=3600,
     timezone="UTC",
     enable_utc=True,
 )
-from app.worker import beat_schedule
+
+# 3) Importar o beat_schedule (só agora!)
+from app.worker.beat_schedule import beat_schedule
+
+# 4) Aplicar o beat_schedule
+celery.conf.beat_schedule = beat_schedule
